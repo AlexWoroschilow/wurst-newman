cb4de257bfda5cd3aaee5dd00a95830d1c0b34b5
Alex Woroschilow <alex.woroschilow@gmail.com>
2016-01-13 17:38:18 +0100

    =add missed library

--- graphtocluster.sh
+++ graphtocluster.sh
@@ -10,6 +10,7 @@
 ROOT=$(pwd)
 
 export LD_LIBRARY_PATH=${ROOT}/vendor/zlog/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${ROOT}/vendor/igraph/lib:$LD_LIBRARY_PATH
 #valgrind --leak-check=full 
 ./Debug/graphtocluster -c ./graphtocluster.cnf
 
